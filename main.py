ebe948ccc1fa74a85dced538418b31d99bce4403
sesemechicken <33507035+sesemechicken@users.noreply.github.com>
2023-12-16 18:14:08 -0600

    Comments main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 def decode(message_file):
+    #file handling
     sortedlist = []
     handle = open(message_file)
     for line in handle:
         linetext = line.replace("\n", "")
         sortedlist.append(linetext)
     handle.close()
-
+    
+    #sort list
     sortedlist.sort(key=lambda text: int(text.split(" ")[0]))
-
+    
+    #loop array
     index = 0
     formatiterator = 1
     decodedstring = ""
     for text in sortedlist:
+        #filter then format
         if index % formatiterator == 0:
             decodedstring += text.split(" ")[1]+" "
             formatiterator += 1
